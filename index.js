ec88c8f45f8e31116d7bd478c8dbcccc14e80d90
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-06-05 05:22:09 +0600

    create get api for users data

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ async function run() {
         res.send(result);
     })
 
+    app.get("/users",async(req,res)=>{
+        const result=await userCollection.find().toArray();
+        res.send(result);
+    })
+
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
