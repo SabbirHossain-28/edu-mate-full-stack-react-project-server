736af1d0d26904dcb39899a77415e063ed9150d0
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-06-05 04:26:12 +0600

    connent the server with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const app=express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const port=process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+// DB_user:eduMateDB
+// DB_pass:HrgVyVA5htbicXZ2
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7nkbk6a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/",(req,res)=>{
     res.send("EduMate server is running perfectly");
